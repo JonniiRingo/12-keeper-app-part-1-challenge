b764c15f7060874ba7619340a8de85e70cc128a9
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-19 20:33:49 -0700

    Set up project file structure and added correct React18 boiler plate to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -11,8 +11,12 @@
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+
+root.render("<App />"); 
